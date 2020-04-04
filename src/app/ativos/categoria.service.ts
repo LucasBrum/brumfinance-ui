769ebe4680af3e60a14d11b7e4f059449307c9c6
lucas.brum <lucas.brum.ext@ipiranga.ipiranga>
2020-04-04 08:04:42 -0300

    Criando servico de Categorias e o metodo que lista as categorias existentes

---
+++ src/app/ativos/categoria.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Promise<any> {
+    return this.http.get(`${this.categoriasUrl}`)
+      .toPromise()
+      .then(response => response);
+  }
+
+}
